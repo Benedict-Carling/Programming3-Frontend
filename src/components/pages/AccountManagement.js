d2c93aead840565c50e114b300bba46efdca14ed
Benedict Carling <benedict.carling18@imperial.ac.uk>
2020-12-08 15:31:09 +0000

    adds register component for development

--- src/components/pages/AccountManagement.js
+++ src/components/pages/AccountManagement.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+
+import Register from "../AccountManagement/Register";
 
 export default function AccountManagement(props) {
   return (
@@ -11,14 +14,13 @@ export default function AccountManagement(props) {
       padding={6}
       spacing={2}
     >
-      <Grid item xs={12} sm={4}>
-        HI
+      <Grid item xs={12} sm={6}>
+        <Card type="outlined">
+          <Register />
+        </Card>
       </Grid>
-      <Grid item xs={12} sm={4}>
+      <Grid item xs={12} sm={6}>
         Hello
-      </Grid>
-      <Grid item xs={12} sm={4}>
-        howdy
       </Grid>
     </Grid>
   );
