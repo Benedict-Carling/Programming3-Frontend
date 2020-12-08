9fa94ecbc4c25f6a21b93a882edbcb7e701d72a7
LorenaGarcia-Foncillas <57544172+LorenaGarcia-Foncillas@users.noreply.github.com>
2020-12-08 17:38:42 +0000

    Forgot password button

---
+++ src/components/auth/ForgotPassword.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+export default function ForgotPassword(props) {
+
+  return (
+    <div>
+      <Dialog open={props.open} onClose={props.handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Forgot your password</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Insert your email address below. Once you click the submit button a new password 
+            will be send to your email.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={props.handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={props.handleClose} color="primary">
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
