68f68079d266208419e61f45dcc9cc6a8508cfb1
LorenaGarcia-Foncillas <57544172+LorenaGarcia-Foncillas@users.noreply.github.com>
2020-12-13 10:50:46 +0100

    Organised files for Login Components

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Login.css";
 import { Grid } from "@material-ui/core";
-import ImageDynamic from './ImageDynamic';
-import LoginForm from './LoginForm';
+import ImageDynamic from '../LoginComponents/ImageDynamic';
+import LoginForm from '../LoginComponents/LoginForm';
 
 export default function Login() {
 
