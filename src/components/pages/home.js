d35437206eda34c004836702ae0208047be95094
LorenaGarcia-Foncillas <57544172+LorenaGarcia-Foncillas@users.noreply.github.com>
2020-12-13 17:47:43 +0100

    Update home page with dynamic image

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
-import GetTable from '../Table/getTable'
-import covidStats from "./covidStats.png";
+import ImageDynamic from "../CovidStatsImg/ImageDynamic";
 import "./home.css";
-import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -18,43 +16,50 @@ export default function Home() {
 return(
     <div className="Home">
         <Grid container spacing={2} padding='5px' >
-            <Grid item xs={12} sm={9} style={{paddingTop: 0, paddingBottom: 0}}>
+            <Grid item xs={12} style={{paddingTop: 0, paddingBottom: 0}}>
                  <header className="Home-MainTitle">
-                    <h1 className="Home-title">REACT 2</h1>
+                    <h1>
+                      REACT 2
+                    </h1>
                 </header>
             </Grid>
-            <Grid item xs={12} sm={9}>
-                <img src={covidStats} className="Home-picture" alt="covidStats" />
-            </Grid>
-            <Grid item xs={12} sm={9}>
-                 <header className="Home-header">
-                    <h1 className="Home-title">Choose the round of tests you would like to have a look at:</h1>
-                </header>
+
+            <Grid item xs={12} sm={6}>
+              <h1 className="Home-header">
+                Choose the round of tests you would like to have a look at:
+              </h1>
             </Grid>
             
-            <Grid item xs={12} sm={9} style={{marginLeft: 50}}>
-              <List component="nav" className="Home-Rounds" aria-label="mailbox folders">
+            <Grid item xs={12} sm={6} style={{marginLeft: 50}}>
+              <List component="nav" className="Home-Rounds" >
                 <Link style={{color:'inherit'}} to="/Data">
-                <ListItem button >
-                <ListItemText primary="Round 1" />  
-                </ListItem>
+                  <ListItem button >
+                    <ListItemText primary="Round 1" />  
+                  </ListItem>
                 </Link>
+
                 <Divider />
+
                 <ListItem button divider>
                  <ListItemText primary="Round 2A" />
                 </ListItem>
-                 <ListItem button>
+
+                <ListItem button>
                  <ListItemText primary="Round 2B" />
                 </ListItem>
+
                 <Divider light />
+
                 <ListItem button>
                  <ListItemText primary="Add new Round" />
                 </ListItem>
               </List>
             </Grid>
-      </Grid>
-      </div>
-        
-    )
 
+            <Grid item xs={12} sm={5}>
+                <ImageDynamic className="Home-picture" />
+            </Grid>
+        </Grid>
+      </div>  
+    );
 }
