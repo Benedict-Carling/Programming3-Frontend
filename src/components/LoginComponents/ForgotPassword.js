3883edf3b2f4aee0e3403423ecd6e5c4d651ea4e
LorenaGarcia-Foncillas <57544172+LorenaGarcia-Foncillas@users.noreply.github.com>
2021-01-09 17:11:44 +0000

    Forgot Pasword with mailing functionality

--- src/components/LoginComponents/ForgotPassword.js
+++ src/components/LoginComponents/ForgotPassword.js
@@ -1,39 +1,32 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Emailsender from './Emailsender';
 
 export default function ForgotPassword(props) {
-
+  
   return (
     <div>
       <Dialog open={props.open} onClose={props.handleClose} aria-labelledby="form-dialog-title">
         <DialogTitle id="form-dialog-title">Forgot your password</DialogTitle>
         <DialogContent>
           <DialogContentText>
-            Insert your email address below. Once you click the submit button a new password 
-            will be send to your email.
+            If you have forgotten your password, email your supervisor. By clicking on 'SEND EMAIL', 
+            your preferred mail application would automatically open.
           </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-          />
         </DialogContent>
+
         <DialogActions>
           <Button onClick={props.handleClose} color="primary">
-            Cancel
+            CANCEL
           </Button>
-          <Button onClick={props.handleClose} color="primary">
-            Submit
-          </Button>
+
+          <Emailsender email="supervisor@email.com" subject="Forgot Password" body="Please could you reset my account?">
+          </Emailsender>
         </DialogActions>
       </Dialog>
     </div>
