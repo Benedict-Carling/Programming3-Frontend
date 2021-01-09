0c40d146f35a02e068b5482b2d6d9204c2e58788
LorenaGarcia-Foncillas <57544172+LorenaGarcia-Foncillas@users.noreply.github.com>
2021-01-09 12:59:44 +0000

    Delete page trial

--- src/components/AccountManagement/DeleteUsers.js
+++ src/components/AccountManagement/DeleteUsers.js
@@ -1,9 +1,10 @@
-import * as React from "react";
+import React, { useContext } from "react";
 import { useState } from "react";
 import { DataGrid } from "@material-ui/data-grid";
 import Axios from "axios";
 import { Button } from "@material-ui/core";
 import {Grid} from "@material-ui/core";
+import UserContext from "../../context/UserContext";
 
 const columns = [
   { field: "id", headerName: "ID", width: 200 },
@@ -11,10 +12,16 @@ const columns = [
   { field: "accountType", headerName: "Account Type", width: 200 },
 ];
 
+let selectedArray = [];
+
 function selectedRow(row) {
+  selectedArray = row;
   console.log(row);
-  console.log("hi");
-}
+  console.log(row.data.id);
+  console.log(row.data.email);
+  console.log(selectedArray);
+  return {selectedArray};
+};
 
 function process(entry) {
   return {
@@ -22,10 +29,11 @@ function process(entry) {
     email: entry.email,
     accountType: entry.accountType,
   };
-}
+};
 
 export default function DataTable() {
   const [state, setstate] = useState([]);
+  const {userData, setUserData} = useContext(UserContext);
 
   React.useEffect(() => {
     (async () => {
@@ -34,6 +42,26 @@ export default function DataTable() {
       setstate(processed);
     })();
   }, []);
+
+  function DeleteFunc() {
+    const config = {
+      headers: {"x-auth-token":userData.token}
+    };
+
+    const UserDeleteId = {
+      "accountIDToDelete": selectedArray.data.id
+    };
+
+    console.log(selectedArray.data.id);
+    
+    console.log(UserDeleteId);
+
+    Axios.delete(`http://localhost:5000/users/delete/`, UserDeleteId, config)
+    .then(res => {
+      console.log(res);
+      console.log(res.data);
+    }); 
+  };
 
   return (
     <div style={{ height: "650px", width: "100%" }}>
@@ -44,7 +72,7 @@ export default function DataTable() {
         alignItems="center"
         >
           <Grid item>
-            <Button variant="contained" color="secondary" type="submit" >
+            <Button variant="contained" color="secondary" type="submit" onClick={DeleteFunc}>
               DELETE
             </Button>
           </Grid>
@@ -55,7 +83,8 @@ export default function DataTable() {
         columns={columns}
         pageSize={10}
         checkboxSelection
-        onRowSelected={(row) => selectedRow(row)}
+        selecter
+        onRowSelected = {selected => {selectedRow(selected)}}
       />
     </div>
   );
