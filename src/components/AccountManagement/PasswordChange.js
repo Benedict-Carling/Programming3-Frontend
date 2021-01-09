d02a1adfb78298014abcf800a7fdbc9bf7b558df
mm10118 <mireia.munoz-rojo18@imperial.ac.uk>
2021-01-09 10:21:28 +0000

    design of page

---
+++ src/components/AccountManagement/PasswordChange.js
@@ -0,0 +1,92 @@
+import "../pages/profile.css";
+import React, { useState, useContext } from "react";
+import Button from "@material-ui/core/Button";
+import SuccessAlert from "./SuccessAlert";
+import ErrorAlert from "./ErrorAlert";
+
+
+export default function PasswordChange() {
+    const [password, setPassword] = useState("");
+    const [passwordCheck, setPasswordCheck] = useState("");
+    const [openSuccessMessage, setOpenSuccessMessage] = useState(false);
+    const [openErrorMessage, setOpenErrorMessage] = useState(false);
+    const [ErrorMessage, setErrorMessage] = useState("");
+
+    const { userData, setUserData } = useContext(UserContext); 
+
+const handleChange = (event) => {
+    setAccountType(event.target.value);
+  };
+
+  const submit = async (e) => {
+    e.preventDefault();
+   
+   await Axios.post(
+    "http://localhost:5000/users/changePassword",
+    newUser
+   )
+    .then(response=> {
+      console.log(response);
+      setOpenSuccessMessage(true);
+    })
+    .catch(error=> {
+      setOpenErrorMessage(true);
+      setErrorMessage(error.response.data.msg); // we get the error message from the post request made in the backend
+    })
+    
+  };
+    function validateForm() {
+        // makes sure that there are no empty fields when you submit a registration
+        return (
+          password.length > 0 &&
+          passwordCheck.length > 0 
+        );
+      }
+return(
+    <div>
+        <form>
+            <h1 className="Profile-header">
+                Change  password:
+            </h1>
+            <label className="Profile-password">
+                <h1 htmlFor="register-password">New Password:</h1>
+            </label>
+
+            <input
+                className="Textfield"
+                
+                id="register-password"
+                type="password"
+            />
+
+            <label className="Profile-passwordCheck">
+                <h1 htmlFor="register-passwordCheck">Password verification</h1>
+            </label>
+
+            <input
+                className="Textfield"
+                
+                id="register-passwordCheck"
+                type="password"
+                placeholder="Verify password"
+            />
+             <Button
+                onClick={submit}
+                variant="contained"
+                color="secondary"
+                type="submit"
+                value="Register"
+                disabled={!validateForm()} // disables the button unless all the conditions stated in the function validateForm() are met
+                >
+                Change Password
+             </Button>
+             
+            </form>
+            <Grid className="Alert" item xs={5} sm ={5} md={7} lg={12} >
+                <SuccessAlert setOpen={setOpenSuccessMessage} open={openSuccessMessage} />
+                <ErrorAlert setOpen={setOpenErrorMessage} open={openErrorMessage} Error ={ErrorMessage} />
+            </Grid>
+            </div>
+
+);
+}
--- src/components/Table/EditTable.js
+++ src/components/Table/EditTable.js
@@ -19,7 +19,8 @@ export default function EditTable(props) {
     },
     formControl: {
       margin: theme.spacing(1),
-      minWidth: 120,
+      minWidth: 285,
+      paddingLeft: -50,
     },
   }));
   const { userData, setUserData } = useContext(UserContext);
@@ -123,7 +124,8 @@ export default function EditTable(props) {
       <TextField
         id="idofclicked"
         label="ID"
-        variant="filled"
+        variant="outlined"
+        fullWidth= '20ch' 
         value={props.selectedID}
         InputProps={{classes:labelClasses}}
       />
@@ -134,7 +136,8 @@ export default function EditTable(props) {
         label="Expert Comment"
         multiline
         rows={6}
-        variant="filled"
+        fullWidth= '20ch' 
+        variant="outlined"
       />
       
       <FormControl className={classes.formControl}>
@@ -147,7 +150,7 @@ export default function EditTable(props) {
           onOpen={handleOpen}
           onChange={handleChange}
           value={props.expertInterpretation}
-          variant="filled"
+          variant="outlined"
         >
           
           <MenuItem value={"Test: uR; Validity: -; IgG:-"}>Test: uR; Validity: -; IgG:-</MenuItem>
