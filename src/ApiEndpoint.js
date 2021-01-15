5140c7b07dc82012415c1ca9b383b033d655a6bb
Benedict Carling <53439684+Benedict-Carling@users.noreply.github.com>
2021-01-15 12:26:05 +0000

    adds comments to APIEndpoint

--- src/ApiEndpoint.js
+++ src/ApiEndpoint.js
@@ -1,2 +1,5 @@
-//export const ApiEndpoint = "https://programmingbackend3.herokuapp.com/";
-export const ApiEndpoint = "http://127.0.0.1:8000/";
+// The first URL is for the hosted backed for testing only the frontend,
+// The second URL is for the local backend, use if testing backend from the frontend
+
+export const ApiEndpoint = "https://programmingbackend3.herokuapp.com/";
+//export const ApiEndpoint = "http://127.0.0.1:8000/";
