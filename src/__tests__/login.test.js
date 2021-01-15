9e0f525a76692439db39059a7aec5481037344d6
Benedict Carling <53439684+Benedict-Carling@users.noreply.github.com>
2021-01-15 12:21:40 +0000

    adds login.js unittest

---
+++ src/__tests__/login.test.js
@@ -0,0 +1,18 @@
+import Login from "../components/Login/Components/LoginForm";
+import Button from "@material-ui/core/Button";
+import React from "react";
+import { mount } from "enzyme";
+
+describe("Home Component", () => {
+  it("Home component renders successfully and has styled title of React 2", () => {
+    const wrapper = mount(<Login />);
+    console.log(wrapper);
+    expect(
+      wrapper.contains(
+        <label className="Label" htmlFor="login-email">
+          Email
+        </label>
+      )
+    ).toBe(true);
+  });
+});
