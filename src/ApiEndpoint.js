915c7ca055a0a7c98f0e32bf36da8d0e717609d2
Benedict Carling <53439684+Benedict-Carling@users.noreply.github.com>
2021-01-15 13:24:08 +0000

    changes local port

--- src/ApiEndpoint.js
+++ src/ApiEndpoint.js
@@ -2,4 +2,4 @@
 // The second URL is for the local backend, use if testing backend from the frontend
 
 export const ApiEndpoint = "https://programmingbackend3.herokuapp.com/";
-//export const ApiEndpoint = "http://127.0.0.1:8000/";
+//export const ApiEndpoint = "http://127.0.0.1:5000/";
